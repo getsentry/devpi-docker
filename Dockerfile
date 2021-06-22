e45d519cd7cf15bf81800c6cc829504c1319bbc1
Anton Ovchinnikov <anton@tonyo.info>
2021-06-22 17:41:28 +0200

    Add Dockerfile & docker-entrypoint.sh

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM python:3.9-alpine
+
+RUN apk add --update gcc python3-dev libffi-dev musl-dev
+RUN pip install devpi-server==5.5.1 devpi-client==5.2.2 ruamel.yaml==0.17.9
+
+ENV DEVPISERVER_SERVERDIR=/srv/devpi
+
+COPY docker-entrypoint.sh /docker-entrypoint.sh
+ENTRYPOINT ["/docker-entrypoint.sh"]
---
+++ docker-entrypoint.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+set -eux
+
+if [ -z "${DEVPISERVER_HOST:-}" ]; then
+    DEVPISERVER_HOST="0.0.0.0"
+fi
+
+if [ -z "${DEVPISERVER_PORT:-}" ]; then
+    DEVPISERVER_PORT="3141"
+fi
+
+if [ ! -f "${DEVPISERVER_SERVERDIR:-}/.nodeinfo" ]; then
+    echo "Initializing devpi..."
+    devpi-init
+else
+    echo "Already initialized, skipping."
+fi
+
+exec devpi-server --host="${DEVPISERVER_HOST}" --port="${DEVPISERVER_PORT}" $@
