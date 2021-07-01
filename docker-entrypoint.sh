1d2612bd3b9f2a7ce37cab5768c4dd4f017c0f36
Anton Ovchinnikov <anton@tonyo.info>
2021-07-01 17:45:17 +0200

    feat: Set random root password on startup

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,19 +1,42 @@
 #!/usr/bin/env sh
-set -eux
+set -eu
+
+set_password() {
+    expect <<-'EOF'
+        set timeout -1
+
+        spawn /usr/local/bin/devpi-passwd root
+
+        expect "enter password" { send "$env(DEVPISERVER_PASSWORD)\r" }
+        expect "repeat password" { send "$env(DEVPISERVER_PASSWORD)\r" }
+
+        expect "committed: keys:"
+        wait
+EOF
+}
 
 if [ -z "${DEVPISERVER_HOST:-}" ]; then
-    DEVPISERVER_HOST="0.0.0.0"
+    export DEVPISERVER_HOST="0.0.0.0"
 fi
 
 if [ -z "${DEVPISERVER_PORT:-}" ]; then
-    DEVPISERVER_PORT="3141"
+    export DEVPISERVER_PORT="3141"
+fi
+
+if [ -z "${DEVPISERVER_PASSWORD:-}" ]; then
+    # Generate a random password if not provided
+    export DEVPISERVER_PASSWORD=$(</dev/urandom tr -dc _A-Z-a-z-0-9 | head -c32)
 fi
 
 if [ ! -f "${DEVPISERVER_SERVERDIR:-}/.nodeinfo" ]; then
     echo "Initializing devpi..."
     devpi-init
+
+    # Set the root password
+    set_password
 else
     echo "Already initialized, skipping."
 fi
 
+set -x
 exec devpi-server --restrict-modify root --host="${DEVPISERVER_HOST}" --port="${DEVPISERVER_PORT}" $@
