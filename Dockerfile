bf346122c4ad3fa4adb1e28752e2ccdbcbcdd5a8
Anton Ovchinnikov <anton@tonyo.info>
2021-07-01 18:38:16 +0200

    feat: Set random root password on startup (#2)

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM python:3.9-alpine
 
-RUN apk add --update gcc python3-dev libffi-dev musl-dev
+RUN apk add --update gcc python3-dev libffi-dev musl-dev expect
 RUN pip install --no-cache-dir \
     devpi-server==5.5.1 devpi-client==5.2.2 ruamel.yaml==0.17.9
 
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,19 +1,25 @@
 #!/usr/bin/env sh
-set -eux
+set -eu
 
 if [ -z "${DEVPISERVER_HOST:-}" ]; then
-    DEVPISERVER_HOST="0.0.0.0"
+    export DEVPISERVER_HOST="0.0.0.0"
 fi
 
 if [ -z "${DEVPISERVER_PORT:-}" ]; then
-    DEVPISERVER_PORT="3141"
+    export DEVPISERVER_PORT="3141"
+fi
+
+if [ -z "${DEVPISERVER_PASSWORD:-}" ]; then
+    # Generate a random password if not provided
+    export DEVPISERVER_PASSWORD=$(</dev/urandom tr -dc _A-Z-a-z-0-9 | head -c32)
 fi
 
 if [ ! -f "${DEVPISERVER_SERVERDIR:-}/.nodeinfo" ]; then
     echo "Initializing devpi..."
-    devpi-init
+    devpi-init --root-passwd "${DEVPISERVER_PASSWORD}"
 else
     echo "Already initialized, skipping."
 fi
 
+set -x
 exec devpi-server --restrict-modify root --host="${DEVPISERVER_HOST}" --port="${DEVPISERVER_PORT}" $@
